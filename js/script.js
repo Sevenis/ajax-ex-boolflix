cf2137abe82787e125d5ca8f3982fb38705ede9d
Pietro Sette <pietrosette@gmail.com>
2020-09-03 17:19:46 +0200

    milestone 2

--- js/script.js
+++ js/script.js
@@ -5,9 +5,10 @@ $(document).ready(function(){
         var thisFilm = $('#search-text').val();
         $('#search-text').val('');
         $('.movies-container').empty();
-        //richiama funzione search
-        search(thisFilm, "https://api.themoviedb.org/3/search/movie");
-        search(thisFilm, "https://api.themoviedb.org/3/search/tv" );
+        //richiama funzione search sia per API movie che per API serie tv
+        //e passa una stringa che identifica l'API di riferimento
+        search(thisFilm, "https://api.themoviedb.org/3/search/movie", "Movie");
+        search(thisFilm, "https://api.themoviedb.org/3/search/tv", "TV Series");
     });
     //search tramite tasto invio
     $('#search-text').on('keypress',function(e) {
@@ -16,16 +17,17 @@ $(document).ready(function(){
         var thisFilm = $('#search-text').val();
         $('#search-text').val('');
         $('.movies-container').empty();
-        //richiama funzione search
-        search(thisFilm, "https://api.themoviedb.org/3/search/movie");
-        search(thisFilm, "https://api.themoviedb.org/3/search/tv" );
+        //richiama funzione search sia per API movie che per API serie tv
+        //e passa una stringa che identifica l'API di riferimento
+        search(thisFilm, "https://api.themoviedb.org/3/search/movie", "Movie");
+        search(thisFilm, "https://api.themoviedb.org/3/search/tv", "TV Series");
         }
     });
 });
 
-//** FUNZIONI
+//** FUNZIONI **//
 //funzione di ricerca tramite chiamata AJAX
-function search(data, url) {
+function search(data, url, type) {
     $.ajax({
         url: url,
         method: "GET",
@@ -34,32 +36,36 @@ function search(data, url) {
                     'query': data,
                     'language': 'it-IT'
                 },
-
         success: function (risposta) {
-            print(risposta);
+            print(risposta, type);
+            console.log(type);
+        },
+        error: function (richiesta, stato, errori) {
+            alert("E' avvenuto un errore. " + errore);
         }
     });
 }
 
 //funzione di stampa tramite append del risultato
-function print(risultato){
+function print(risultato, type){
     var source = $("#entry-template").html();
     var template = Handlebars.compile(source);
 
     for (var i = 0; i < risultato.results.length; i++){
-        // var context = risultato.results[i];
         var context = {
             title: risultato.results[i].title || risultato.results[i].original_name,
             original_title: risultato.results[i].original_title,
             original_language: risultato.results[i].original_language,
             vote_average: stars(risultato.results[i].vote_average),
-            type: type(risultato.results[i].original_title)
+            type: type
         }
         var html = template(context);
         $('.movies-container').append(html);
     }
 }
 
+//funzione che restituisce il valore del voto in stelle
+//approssimato per eccesso
 function stars(num){
     var vote = Math.ceil(num/2);
     var star = '';
@@ -73,10 +79,12 @@ function stars(num){
     return star;
 }
 
-function type(data){
-    if (data != null){
-        return 'Movie'
-    } else {
-        return 'Serie TV'
-    }
-}
+//funzione alternativa che gestisce il tipo tra SerieTV e Movie
+//sulla base di un campo discrimante di un'api rispetto all'altra
+// function type(data){
+//     if (data != null){
+//         return 'Movie'
+//     } else {
+//         return 'Serie TV'
+//     }
+// }
